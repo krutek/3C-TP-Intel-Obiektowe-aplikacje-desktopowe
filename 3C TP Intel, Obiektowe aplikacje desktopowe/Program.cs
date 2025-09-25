8dcfcbccaaf75dc2f31cb7d8aadf43ed4fc26203
blampricht <blampricht@10572-TPS>
2025-09-25 14:23:49 +0200

    Wszystko aż do funkcji

--- 3C TP Intel, Obiektowe aplikacje desktopowe/Program.cs
+++ 3C TP Intel, Obiektowe aplikacje desktopowe/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,7 +24,7 @@ namespace _3C_TP_Intel__Obiektowe_aplikacje_desktopowe
             // Zakomentowany linie kodu nie będą widziane przez kompilator
 
             //byte by = 0; // 0 do 255
-            //short s = 0;
+            //short nazwa = 0;
             //int b = 0;
             //long l = 0l;
 
@@ -48,20 +50,20 @@ namespace _3C_TP_Intel__Obiektowe_aplikacje_desktopowe
             //Console.WriteLine(znak); //  <-  Wyświetla tekst na konsoli
             //Console.Write(znak);
 
-            //int wiek = 18;
+            int wiek = 18;
 
-            //if (wiek >= 18)
-            //{
-            //    Console.WriteLine("Jesteś pełnoletni");
-            //}
-            //else if (wiek == 16 || wiek != 17)
-            //{
+            if (wiek >= 18)
+            {
+                //Console.WriteLine("Jesteś pełnoletni");
+            }
+            else if (wiek == 16 || wiek != 17)
+            {
 
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Nie jesteś pełnoletni");
-            //}
+            }
+            else
+            {
+                //Console.WriteLine("Nie jesteś pełnoletni");
+            }
 
 
             //string liczba1 = "5";
@@ -74,6 +76,9 @@ namespace _3C_TP_Intel__Obiektowe_aplikacje_desktopowe
             ///////////////////////////////////////////////////
             ///
             int[] tablicaIntow = new int[5]; // tablica 5 elementowa
+
+          
+
             int[] innaTablica = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // tablica inicjalizowana
             //typ [] nazwa = new typ[rozmiar];
             string[] tablicaStringow = new string[10]; // tablica 10 elementowa
@@ -88,7 +93,7 @@ namespace _3C_TP_Intel__Obiektowe_aplikacje_desktopowe
             }
             for (int i = 0; i < tablicaIntow.Length; i++)
             {
-                Console.WriteLine(tablicaIntow[i]);
+                //Console.WriteLine(tablicaIntow[i]);
             }
             //int <-integer
             List<int> listaIntow = new List<int>();
@@ -98,7 +103,7 @@ namespace _3C_TP_Intel__Obiektowe_aplikacje_desktopowe
 
             foreach (var elementListy in listaImion)
             {
-                Console.WriteLine(elementListy);
+                //Console.WriteLine(elementListy);
             }
 
 
@@ -139,18 +144,18 @@ namespace _3C_TP_Intel__Obiektowe_aplikacje_desktopowe
             /// jesli user nie poda liczby to zmienna czyUzytkownikPodalLiczbe będzie false
             /// i nie wywali nam błędu
 
-            while (NiechPetrlaSieWykonuje)
-            {
+           // while (NiechPetrlaSieWykonuje)
+           // {
                 //int wiekUzytkownia1 = Convert.ToInt32(Console.ReadLine());
-                bool czyUzytkownikPodalLiczbe = int.TryParse(Console.ReadLine(), out int wiekUzytkownia);
+               // bool czyUzytkownikPodalLiczbe = int.TryParse(Console.ReadLine(), out int wiekUzytkownia);
 
-                if (czyUzytkownikPodalLiczbe && wiekUzytkownia >= 18) 
-                {
-                    Console.WriteLine( "jesteś pełnoletni" );
-                    NiechPetrlaSieWykonuje = false;
-                }
-                Console.WriteLine("nie jesteś");
-            }
+                //if (czyUzytkownikPodalLiczbe && wiekUzytkownia >= 18) 
+                //{
+                //    Console.WriteLine( "jesteś pełnoletni" );
+                //    NiechPetrlaSieWykonuje = false;
+                //}
+                ////Console.WriteLine("nie jesteś");
+           // }
 
             /// tablica wielowymiarowa
             int[,] macierz = new int[5, 5]
@@ -159,7 +164,7 @@ namespace _3C_TP_Intel__Obiektowe_aplikacje_desktopowe
                 {4, 5, 6, 5 ,6},
                 {7, 8, 9, 5 ,6},
                 {7, 8, 9, 5 ,6},
-                {7, 8, 9, 50 ,61}
+                {7, 8, 9, 50 ,6}
             };
             var suma = 0;
             for (int i = 0; i < macierz.GetLength(0); i++)        // liczba wierszy
@@ -170,7 +175,7 @@ namespace _3C_TP_Intel__Obiektowe_aplikacje_desktopowe
                         suma += macierz[i, j];
                 }
             }
-            Console.WriteLine(suma);
+            //Console.WriteLine(suma);
 
             /// Tabiica postrzępiona - jagged array
             int[][] trojkat = new int[3][];
@@ -195,8 +200,170 @@ namespace _3C_TP_Intel__Obiektowe_aplikacje_desktopowe
             umiejetnosc.Add("Skradanie sie", 2);
 
 
-            foreach (var para in umiejetnosc)
-                Console.WriteLine($"{para.Key} = {para.Value}");
+            // foreach (var para in umiejetnosc)
+            // Console.WriteLine($"{para.Key} = {para.Value}");
+
+
+            ///Obsługa plików
+            ///na konsoli ma się wyświetlić opcja wyboru zapis czy odczny, wyjdź
+            /// user podaje 1 lub 2 (zapis Lub odczyt)
+            /// po kliknieciu 1 user ma wpisac jakiś tekst
+            /// program ma zapisać do pliku write.txt
+            /// po kliknieciu 2 program ma odczytać z pliku readme.txt
+            /// i wyrzucić tekst na konsolę
+            /// wszystko niech się dzieje w pętli tak by po odczycie lub zapisie
+            /// apilkacja kontynuowała działanie
+            /// po kliknieciu 3 program ma się zakończyć
+            //using (StreamWriter writetext = new StreamWriter("write.txt"))
+            //{
+            //    writetext.WriteLine("writing in text file");
+            //}
+
+            //using (StreamReader readtext = new StreamReader("readme.txt"))
+            //{
+            //    string readText = readtext.ReadLine();
+            //}
+
+
+            //////
+            ///zaj 3
+            //int a = 10, b = 3;
+
+            //// Operatory arytmetyczne
+            //Console.WriteLine("=== Operatory arytmetyczne ===");
+            //Console.WriteLine($"{a} + {b} = {a + b}");
+            //Console.WriteLine($"{a} - {b} = {a - b}");
+            //Console.WriteLine($"{a} * {b} = {a * b}");
+            //Console.WriteLine($"{a} / {b} = {a / b}");   // dzielenie całkowite
+            //Console.WriteLine($"{a} % {b} = {a % b}");   // reszta z dzielenia
+
+            //// Operatory porównania
+            //Console.WriteLine("\n=== Operatory porównania ===");
+            //Console.WriteLine($"{a} > {b}  -> {a > b}");
+            //Console.WriteLine($"{a} < {b}  -> {a < b}");
+            //Console.WriteLine($"{a} >= {b}  -> {a > b}");
+            //Console.WriteLine($"{a} <= {b}  -> {a < b}");
+            //Console.WriteLine($"{a} == {b} -> {a == b}"); 
+            //Console.WriteLine($"{a} != {b} -> {a != b}");
+            ////! <- negacji
+
+            //// Operatory logiczne
+            //bool x = true, y = false;
+            //Console.WriteLine("\n=== Operatory logiczne ===");
+            //Console.WriteLine($"{x} && {y} -> {x && y}"); // AND
+            //Console.WriteLine($"{x} || {y} -> {x || y}"); // OR
+            //Console.WriteLine($"!{x} -> {!x}");           // NOT
+
+            //// Operator inkrementacji / dekrementacji
+            //Console.WriteLine("\n=== Inkrementacja / Dekrementacja ===");
+            //int c = 5;
+            //Console.WriteLine($"c = {c}");
+            //Console.WriteLine($"c++ -> {c++} (najpierw zwraca, potem zwiększa)");
+            //Console.WriteLine($"c po c++ = {c}");
+            //Console.WriteLine($"++c -> {++c} (najpierw zwiększa, potem zwraca)");
+
+
+            //Console.WriteLine("Podaj licznik: ");
+            //int licznik = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Podaj mianownik: ");
+            //int mianownik = int.Parse(Console.ReadLine());
+
+
+            //try
+            //{
+            //    int wynik = licznik / mianownik;
+            //    Console.WriteLine($"Wynik dzielenia: {wynik}");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Błąd: próba dzielenia przez zero!");
+            //    Console.WriteLine($"Szczegóły: {ex.Message}");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Blok finally – wykonuje się zawsze (np. zamknięcie pliku/połączenia).");
+            //}
+
+
+            //void WyswietlPowitanie()
+            //{
+            //    Console.WriteLine("Witaj");
+            //}
+            //WyswietlPowitanie();
+            //WyswietlPowitanie();
+
+            int DodajDwieLiczby(int pierwszaLiczba, int drugaLiczba = 10)
+            {
+                int wynik = pierwszaLiczba + drugaLiczba;
+                return wynik;
+            }
+
+   
+
+
+
+
+
+
+
+
+
+
+            void jakasNazwa(string jeden, string dwa = "Warosc albo nawiasy")
+            {
+                Console.WriteLine(jeden + " " + dwa); //konkatenacja
+            }
+
+
+
+            int obliczenie(int liczbaX, int liczbaY)
+            {
+                
+                for (int i=0; i< liczbaY; i++)
+                {
+                    liczbaX = liczbaX * liczbaX;
+                }
+                return liczbaX;
+
+            }
+
+           Console.WriteLine(obliczenie(2, 10));
+
+            //
+            //Stwórz kolekcję do zapamiętwania towarów i ceny 
+            //a potem w funkcji dodawaj do tej kolecji te towary z ceną 
+
+            //Dictionary<string, decimal> produktyICena = new Dictionary<string, decimal>();
+
+            //void DodajProdukt(string nazwa, decimal cena)
+            //{
+            //    if (produktyICena.ContainsKey(nazwa))
+            //    {
+            //        // jeśli do słownika odwołam się po kluczu to dostanę wartość
+            //        produktyICena[nazwa] += cena;
+            //    }
+            //    else
+            //    {
+            //        produktyICena.Add(nazwa, cena);
+            //    }   
+            //}
+
+            //DodajProdukt("Ser", 5);
+            //DodajProdukt("Chleb", 3);
+            //DodajProdukt("Mleko", 4);
+            //DodajProdukt("Mleko", 4);
+
+
+            //foreach (var item in produktyICena)
+            //{
+            //    Console.WriteLine(item.Key + " " + item.Value);
+            //}
+
+
+
+
+
 
 
         }
